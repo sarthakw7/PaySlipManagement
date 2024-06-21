abdc1a09c4ae423e06d73038392485d877e4631e
penumatsa-mounika <penumatchamounika@gmail.com>
2024-06-21 13:06:32 +0530

    Implementations, Interfaces and Controllers in PaySlipManagement.DAl, PaySlipManagement.BAL, PaySlipManagement.API. Given API services in PaySlipManagement.UI

---
+++ PaySlipManagement.BAL/Interfaces/ISalaryBALRepo.cs
@@ -0,0 +1,19 @@
+﻿using PaySlipManagement.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySlipManagement.BAL.Interfaces
+{
+    public interface ISalaryBALRepo
+    {
+        Task<IEnumerable<SalaryMetadata>> GetAllSalaryMetadataAsync();
+
+        Task<SalaryMetadata> GetSalaryMetadataByidAsync(SalaryMetadata _salaryMetadata);
+        Task<bool> CreateSalaryMetadata(SalaryMetadata _salaryMetadata);
+        Task<bool> UpdateSalaryMetadata(SalaryMetadata _salaryMetadata);
+        Task<bool> DeleteSalaryMetadata(SalaryMetadata salaryMetadata);
+    }
+}
