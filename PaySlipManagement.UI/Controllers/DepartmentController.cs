9453195fb1869f81b52cb78047c54a139ca694fe
RahulWhiztekcorp <rahul.whiztekcorp@gmail.com>
2024-06-21 17:27:45 +0530

    Added Department Index.cshtml,Create.cshtml,Details.cshtml,Delete.cshtml,Edit.cshtml

---
+++ PaySlipManagement.UI/Controllers/DepartmentController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaySlipManagement.Common.Models;
+
+namespace PaySlipManagement.UI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private static List<Department> _departments = new List<Department>
+        {
+            new Department { DepartmentId = 1, DepartmentName = "HR" },
+            new Department { DepartmentId = 2, DepartmentName = "Finance" },
+            // Add more departments as needed
+        };
+        // GET: DepartmentController
+        public IActionResult Index()
+        {
+            return View(_departments);
+        }
+
+        // GET: DepartmentController/Details/5
+        public IActionResult Details(int id)
+        {
+            var department = _departments.FirstOrDefault(d => d.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // GET: DepartmentController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                department.DepartmentId = _departments.Max(d => d.DepartmentId) + 1;
+                _departments.Add(department);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: DepartmentController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var department = _departments.FirstOrDefault(d => d.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: DepartmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingDepartment = _departments.FirstOrDefault(d => d.DepartmentId == id);
+                if (existingDepartment == null)
+                {
+                    return NotFound();
+                }
+
+                existingDepartment.DepartmentName = department.DepartmentName;
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: DepartmentController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var department = _departments.FirstOrDefault(d => d.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: DepartmentController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var department = _departments.FirstOrDefault(d => d.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            _departments.Remove(department);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
