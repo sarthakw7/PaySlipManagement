15eba2fed8b66d71f415526d82fccbb7cdda11f8
RahulWhiztekcorp <rahul.whiztekcorp@gmail.com>
2024-06-24 13:07:37 +0530

    created AccountDetails Controller and CRUD views Index.cshtml, Create.cshtml, Details.cshtml, Delete.cshtml, and Edit.cshtml.

---
+++ PaySlipManagement.UI/Controllers/AccountDetailsController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaySlipManagement.Common.Models;
+
+namespace PaySlipManagement.UI.Controllers
+{
+    public class AccountDetailsController : Controller
+    {
+        private static List<AccountDetails> _accountDetails = new List<AccountDetails>
+        {
+            new AccountDetails { Id = 1, Emp_Code = "E001", BankName = "Bank A", BankAccountNumber = 1234567890, UANNumber = 9876543210, PFAccountNumber = "PF001" },
+            // Add more account details as needed
+        };
+
+        public IActionResult Index()
+        {
+            return View(_accountDetails);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var account = _accountDetails.FirstOrDefault(a => a.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(AccountDetails account)
+        {
+            if (ModelState.IsValid)
+            {
+                account.Id = _accountDetails.Max(a => a.Id) + 1 ?? 1;
+                _accountDetails.Add(account);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var account = _accountDetails.FirstOrDefault(a => a.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, AccountDetails account)
+        {
+            if (id != account.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingAccount = _accountDetails.FirstOrDefault(a => a.Id == id);
+                if (existingAccount == null)
+                {
+                    return NotFound();
+                }
+
+                existingAccount.Emp_Code = account.Emp_Code;
+                existingAccount.BankName = account.BankName;
+                existingAccount.BankAccountNumber = account.BankAccountNumber;
+                existingAccount.UANNumber = account.UANNumber;
+                existingAccount.PFAccountNumber = account.PFAccountNumber;
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var account = _accountDetails.FirstOrDefault(a => a.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var account = _accountDetails.FirstOrDefault(a => a.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            _accountDetails.Remove(account);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
