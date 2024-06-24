bc74516e944078da84b7859e16776cbc4546dc11
RahulWhiztekcorp <rahul.whiztekcorp@gmail.com>
2024-06-24 13:02:49 +0530

    Added Auth Views

---
+++ PaySlipManagement.UI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaySlipManagement.Common.Models;
+using PaySlipManagement.UI.Common;
+using PaySlipManagement.UI.Models;
+
+namespace PaySlipManagement.UI.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginAsync(LoginViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
