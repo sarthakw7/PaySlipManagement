e97d609d9e0ad7cf1e43b55ae0914a8acc6ac316
RahulWhiztekcorp <rahul.whiztekcorp@gmail.com>
2024-06-25 16:25:01 +0530

    Added SalaryMetaData views such as Index.cshtml, Create.cshtml, Details.cshtml, Delete.cshtml, and Edit.cshtml

---
+++ PaySlipManagement.UI/Controllers/SalaryMetadataController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaySlipManagement.UI.Controllers
+{
+    public class SalaryMetadataController : Controller
+    {
+        // GET: SalaryMetadataController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SalaryMetadataController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SalaryMetadataController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SalaryMetadataController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SalaryMetadataController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SalaryMetadataController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SalaryMetadataController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SalaryMetadataController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PaySlipManagement.UI/Models/SalaryMetadataViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PaySlipManagement.UI.Models
+{
+    public class SalaryMetadataViewModel
+    {
+        public int? Id { get; set; }
+        public string Emp_Code { get; set; }
+        public string PaySlipForMonth { get; set; }
+        public decimal DaysPaid { get; set; }
+        public decimal AbsentDays { get; set; }
+        public decimal EarnedBasic { get; set; }
+        public decimal HRA { get; set; }
+        public decimal SpecialAllowance { get; set; }
+        public decimal PFEmployeeShare { get; set; }
+        public decimal ProfessionalTax { get; set; }
+        public decimal TDS { get; set; }
+        public decimal EarningTotal { get; set; }
+        public decimal TotalDeductions { get; set; }
+        public decimal NetPay { get; set; }
+    }
+}
