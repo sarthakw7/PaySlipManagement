bca6101c5473dedaa4ccd33154be3326ac3c43a3
penumatsa-mounika <penumatchamounika@gmail.com>
2024-07-31 13:37:08 +0530

    Salary- Pay slips and CTC segregation

---
+++ PaySlipManagement.BAL/Interfaces/ICTCDetailsBALRepo.cs
@@ -0,0 +1,19 @@
+﻿using PaySlipManagement.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySlipManagement.BAL.Interfaces
+{
+    public interface ICTCDetailsBALRepo
+    {
+        Task<IEnumerable<CTCDetails>> GetAllCTCDetailsAsync();
+
+        Task<CTCDetails> GetCTCDetailsByidAsync(CTCDetails _details);
+        Task<bool> CreateCTCDetails(CTCDetails _details);
+        Task<bool> UpdateCTCDetails(CTCDetails _details);
+        Task<bool> DeleteCTCDetails(CTCDetails details);
+    }
+}
---
+++ PaySlipManagement.UI/Models/CTCDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace PaySlipManagement.UI.Models
+{
+    public class CTCDetailsViewModel
+    {
+        public int? Id { get; set; }
+        public string Emp_Code { get; set; }
+        public decimal EarnedBasic { get; set; }
+        public decimal HRA { get; set; }
+        public decimal SpecialAllowance { get; set; }
+        public decimal PFEmployeeShare { get; set; }
+        public decimal ProfessionalTax { get; set; }
+        public decimal TDS { get; set; }
+        public decimal AnnualCTC { get; set; }
+        public decimal CTCMonth { get; set; }
+        public decimal PFEmployerShare { get; set; }
+        public decimal PFEmployerShareAnnual { get; set; }
+        public decimal AnnualGrossPay { get; set; }
+        public decimal MonthGrossPay { get; set; }
+        public decimal OtherAdditions { get; set; }
+        public decimal OtherDeductions { get; set; }
+        public decimal TotalDeductions { get; set; }
+        public decimal NetPay { get; set; }
+    }
+}
