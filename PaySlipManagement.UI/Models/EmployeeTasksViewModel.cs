5fcbc83cf46213dd81b4e96ce3885d5ad610f1da
Bhaskar Rao <bhaskar.rao@whiztekcorp.com>
2024-12-27 16:27:10 +0530

    Implemented Employee Tasks filling sheet functionality in employee portal. Changes in Payslip and Document Controller

--- PaySlipManagement.UI/Models/EmployeeTasksViewModel.cs
+++ PaySlipManagement.UI/Models/EmployeeTasksViewModel.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace PaySlipManagement.Common.Models
+namespace PaySlipManagement.UI.Models
 {
-    public class EmployeeTasks
+    public class EmployeeTasksViewModel
     {
         public int? Id { get; set; }
         public string Emp_Code { get; set; }
-
         [DataType(DataType.Date)]
         public DateTime TaskDate { get; set; }
         public TimeSpan TaskFrom { get; set; }
-        public TimeSpan TaskTo { get; set; } 
+        public TimeSpan TaskTo { get; set; }
         public string TaskDescription { get; set; }
         public int Duration { get; set; }
         public string? Status { get; set; }
+        //public List<EmployeeTasks>? Tasks { get; set; }
     }
 
 }
